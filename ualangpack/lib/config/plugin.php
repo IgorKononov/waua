4ba64a18951e2995d7c3b56d2c3548e3e59997c2
Skeeter <kononov.igor@gmail.com>
2019-07-06 19:01:20 +0300

    Обновлен перевод: CRM 1.4.1.263 Хаб 1.2.4.72 Задачи 1.2.0.92 Логи 1.1.2.0

--- ualangpack/lib/config/plugin.php
+++ ualangpack/lib/config/plugin.php
@@ -3,7 +3,7 @@ return array (
   'name' => 'Webasyst по-украински',    
   'description'  => 'Перевод на украинский язык интерфейса продуктов Webasyst',
   'img' => 'img/ualangpack.png',
-  'version' => '1.1.1',
+  'version' => '1.2',
   'vendor' => '1077750',
   'handlers' => 
   array (
