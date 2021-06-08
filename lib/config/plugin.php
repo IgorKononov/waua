85429f99ea812b15547ddc4b160e74d04c7cc886
IgorKononov <kononov.igor@gmail.com>
2021-06-08 16:29:53 +0300

    config update

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,7 +3,7 @@ return array (
   'name' => 'Webasyst по-украински',    
   'description'  => 'Перевод на украинский язык интерфейса продуктов Webasyst',
   'img' => 'img/ualangpack.png',
-  'version' => '1.6.2',
+  'version' => '1.6.3',
   'vendor' => '1077750',
   'handlers' => 
   array (
