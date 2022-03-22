4cd6ab93713bff37978e564c83659912d969f1f8
Igor Kononov <kononov.igor@gmail.com>
2022-03-22 17:00:54 +0400

    1.6.6 Updates #2: Webasyst and Installer 2.4.2.651 Webasyst Shop-Script 9.0.5.57 Webasyst Shop-Script Yandex.Market plugin Webasyst Team 2.0.2.156 Webasyst Hub 2.0.2.114 Webasyst Helpdesk 1.2.19.101 Webasyst CRM 1.4.14.412 1312 Inc. Tasks 2.4.0 Webasyst Boxberry shipping plugin 1.1.7.60 Webasyst Team Outlook.com Calendar plugin 1.1.1.9 Webasyst Team iCalendar plugin 1.1.1.15 Webasyst Team Google Calendar plugin 1.0.22.26 Webasyst Team CalDAV plugin 1.1.1.27 Webasyst CRM Sipuni plugin 1.0.2.15

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,7 +3,7 @@ return array (
   'name' => 'Webasyst по-украински',    
   'description'  => 'Перевод на украинский язык интерфейса продуктов Webasyst',
   'img' => 'img/ualangpack.png',
-  'version' => '1.6.5',
+  'version' => '1.6.6',
   'vendor' => '1077750',
   'handlers' => 
   array (
